a36ecba98ab92d0a0d418533adc1d22d513e2af0
damstrivandrum <aajalms@gmail.com>
2026-06-05 01:08:59 +0530

    Update exam end time to June 5, 2026

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ window.EXAM_TZ = "Asia/Kolkata";
 
 // ✅ Fixed window (IST)
 window.EXAM_START_IST = "2026-05-29T10:00:00+05:30";
-window.EXAM_END_IST   = "2026-05-29T13:05:00+05:30";
+window.EXAM_END_IST   = "2026-06-05T13:05:00+05:30";
 
 // Parsed milliseconds (used by pages)
 window.EXAM_START_MS = Date.parse(window.EXAM_START_IST);
